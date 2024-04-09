d92ea0344152f83fc58126dd2c10c87e5ee1d86d
PhanHoangLocAG <phanhoangloc2017@gmail.com>
2024-04-09 21:53:32 +0700

    fix

--- spring-boot-hibernate-oracle/src/main/java/com/bezkoder/spring/oracle/controller/LoginController.java
+++ spring-boot-hibernate-oracle/src/main/java/com/bezkoder/spring/oracle/controller/LoginController.java
@@ -2,7 +2,7 @@ package com.bezkoder.spring.oracle.controller;
 
 import com.bezkoder.spring.oracle.common.APIResponse;
 import com.bezkoder.spring.oracle.dto.LoginRequestDTO;
-//import com.bezkoder.spring.oracle.service.LoginService;/
+import com.bezkoder.spring.oracle.service.LoginService;
 import com.bezkoder.spring.oracle.util.JwtUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -14,19 +14,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/api/")
 public class LoginController {
-//    @Autowired
-//    private LoginService loginService;
+    @Autowired
+    private LoginService loginService;
 
     @Autowired
     private JwtUtils jwtUtils;
 
-//    @PostMapping("auth/login")
-//    public ResponseEntity<APIResponse> login(@RequestBody LoginRequestDTO loginRequestDTO ){
-//
-//        APIResponse apiResponse = loginService.login(loginRequestDTO);
-//
-//        return ResponseEntity
-//                .status(apiResponse.getStatus())
-//                .body(apiResponse);
-//    }
+    @PostMapping("auth/login")
+    public ResponseEntity<APIResponse> login(@RequestBody LoginRequestDTO loginRequestDTO ){
+
+        APIResponse apiResponse = loginService.login(loginRequestDTO);
+
+        return ResponseEntity
+                .status(apiResponse.getStatus())
+                .body(apiResponse);
+    }
 }
--- spring-boot-hibernate-oracle/src/main/java/com/bezkoder/spring/oracle/entity/Users.java
+++ spring-boot-hibernate-oracle/src/main/java/com/bezkoder/spring/oracle/entity/Users.java
@@ -1,115 +1,115 @@
-//package com.bezkoder.spring.oracle.entity;
-//
-//import com.bezkoder.spring.oracle.common.APIResponse;
-//import com.bezkoder.spring.oracle.dto.LoginRequestDTO;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.stereotype.Service;
-//
-//import javax.persistence.*;
-//import java.util.Date;
-//
-//@Entity
-//public class Users {
-//    @Id
-//    @GeneratedValue(strategy = GenerationType.AUTO)
-//    @Column(name = "MA_USER")
-//    private Integer ma_user;
-//    @Column(name = "USERNAME")
-//    private String username;
-//    @Column(name = "PASSWORD")
-//    private String password;
-//    @Column(name = "STATUS")
-//    private String status;
-//    @Column(name = "CREATED")
-//    private Date created;
-//    @Column(name = "UPDATED")
-//    private Date updated;
-//    @Column(name = "LEVEL_USER")
-//    private String level_user;
-//    @Column(name = "IOC")
-//    private Integer ioc;
-//    @Column(name = "ID_HSSK")
-//    private Integer id_hssk;
-//    @Column(name = "LEVEL_MOBILE")
-//    private Integer level_mobile;
-//
-//    public Integer getMa_user() {
-//        return ma_user;
-//    }
-//
-//    public void setMa_user(Integer ma_user) {
-//        this.ma_user = ma_user;
-//    }
-//
-//    public String getUsername() {
-//        return username;
-//    }
-//
-//    public void setUsername(String username) {
-//        this.username = username;
-//    }
-//
-//    public String getPassword() {
-//        return password;
-//    }
-//
-//    public void setPassword(String password) {
-//        this.password = password;
-//    }
-//
-//    public String getStatus() {
-//        return status;
-//    }
-//
-//    public void setStatus(String status) {
-//        this.status = status;
-//    }
-//
-//    public Date getCreated() {
-//        return created;
-//    }
-//
-//    public void setCreated(Date created) {
-//        this.created = created;
-//    }
-//
-//    public Date getUpdated() {
-//        return updated;
-//    }
-//
-//    public void setUpdated(Date updated) {
-//        this.updated = updated;
-//    }
-//
-//    public String getLevel_user() {
-//        return level_user;
-//    }
-//
-//    public void setLevel_user(String level_user) {
-//        this.level_user = level_user;
-//    }
-//
-//    public Integer getIoc() {
-//        return ioc;
-//    }
-//
-//    public void setIoc(Integer ioc) {
-//        this.ioc = ioc;
-//    }
-//
-//    public Integer getId_hssk() {
-//        return id_hssk;
-//    }
-//
-//    public void setId_hssk(Integer id_hssk) {
-//        this.id_hssk = id_hssk;
-//    }
-//
-//    public Integer getLevel_mobile() {
-//        return level_mobile;
-//    }
-//
-//    public void setLevel_mobile(Integer level_mobile) {
-//        this.level_mobile = level_mobile;
-//    }
-//}
+package com.bezkoder.spring.oracle.entity;
+
+import com.bezkoder.spring.oracle.common.APIResponse;
+import com.bezkoder.spring.oracle.dto.LoginRequestDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+public class Users {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "MA_USER")
+    private Integer ma_user;
+    @Column(name = "USERNAME")
+    private String username;
+    @Column(name = "PASSWORD")
+    private String password;
+    @Column(name = "STATUS")
+    private String status;
+    @Column(name = "CREATED")
+    private Date created;
+    @Column(name = "UPDATED")
+    private Date updated;
+    @Column(name = "LEVEL_USER")
+    private String level_user;
+    @Column(name = "IOC")
+    private Integer ioc;
+    @Column(name = "ID_HSSK")
+    private Integer id_hssk;
+    @Column(name = "LEVEL_MOBILE")
+    private Integer level_mobile;
+
+    public Integer getMa_user() {
+        return ma_user;
+    }
+
+    public void setMa_user(Integer ma_user) {
+        this.ma_user = ma_user;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public Date getCreated() {
+        return created;
+    }
+
+    public void setCreated(Date created) {
+        this.created = created;
+    }
+
+    public Date getUpdated() {
+        return updated;
+    }
+
+    public void setUpdated(Date updated) {
+        this.updated = updated;
+    }
+
+    public String getLevel_user() {
+        return level_user;
+    }
+
+    public void setLevel_user(String level_user) {
+        this.level_user = level_user;
+    }
+
+    public Integer getIoc() {
+        return ioc;
+    }
+
+    public void setIoc(Integer ioc) {
+        this.ioc = ioc;
+    }
+
+    public Integer getId_hssk() {
+        return id_hssk;
+    }
+
+    public void setId_hssk(Integer id_hssk) {
+        this.id_hssk = id_hssk;
+    }
+
+    public Integer getLevel_mobile() {
+        return level_mobile;
+    }
+
+    public void setLevel_mobile(Integer level_mobile) {
+        this.level_mobile = level_mobile;
+    }
+}
